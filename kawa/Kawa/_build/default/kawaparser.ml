3bc0a7e7e82960bbe5f0a11c7b559d05ad2f3744
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-11-13 11:53:52 +0100

    changement de token

---
+++ kawa/Kawa/_build/default/kawaparser.ml
@@ -0,0 +1,349 @@
+
+module MenhirBasics = struct
+  
+  exception Error
+  
+  let _eRR =
+    fun _s ->
+      raise Error
+  
+  type token = 
+    | WHILE
+    | VOID
+    | VAR
+    | TRUE
+    | THIS
+    | SEMI
+    | RPAR
+    | RETURN
+    | PRINT
+    | NEW
+    | METHOD
+    | MAIN
+    | LPAR
+    | INT of (
+# 9 "kawaparser.mly"
+       (int)
+# 28 "kawaparser.ml"
+  )
+    | IF
+    | IDENT of (
+# 10 "kawaparser.mly"
+       (string)
+# 34 "kawaparser.ml"
+  )
+    | FALSE
+    | EOF
+    | END
+    | ELSE
+    | CLASS
+    | BOOL of (
+# 11 "kawaparser.mly"
+       (bool)
+# 44 "kawaparser.ml"
+  )
+    | BEGIN
+    | ATTRIBUTE
+  
+end
+
+include MenhirBasics
+
+# 1 "kawaparser.mly"
+  
+
+  open Lexing
+  open Kawa
+
+
+# 60 "kawaparser.ml"
+
+type ('s, 'r) _menhir_state = 
+  | MenhirState02 : ('s, _menhir_box_program) _menhir_state
+    (** State 02.
+        Stack shape : .
+        Start symbol: program. *)
+
+  | MenhirState03 : (('s, _menhir_box_program) _menhir_cell1_RETURN, _menhir_box_program) _menhir_state
+    (** State 03.
+        Stack shape : RETURN.
+        Start symbol: program. *)
+
+  | MenhirState08 : (('s, _menhir_box_program) _menhir_cell1_PRINT, _menhir_box_program) _menhir_state
+    (** State 08.
+        Stack shape : PRINT.
+        Start symbol: program. *)
+
+  | MenhirState15 : (('s, _menhir_box_program) _menhir_cell1_instruction, _menhir_box_program) _menhir_state
+    (** State 15.
+        Stack shape : instruction.
+        Start symbol: program. *)
+
+
+and ('s, 'r) _menhir_cell1_instruction = 
+  | MenhirCell1_instruction of 's * ('s, 'r) _menhir_state * (Kawa.instr)
+
+and ('s, 'r) _menhir_cell1_PRINT = 
+  | MenhirCell1_PRINT of 's * ('s, 'r) _menhir_state
+
+and ('s, 'r) _menhir_cell1_RETURN = 
+  | MenhirCell1_RETURN of 's * ('s, 'r) _menhir_state
+
+and _menhir_box_program = 
+  | MenhirBox_program of (Kawa.program) [@@unboxed]
+
+let _menhir_action_1 =
+  fun n ->
+    (
+# 37 "kawaparser.mly"
+        ( Int(n) )
+# 101 "kawaparser.ml"
+     : (Kawa.expr))
+
+let _menhir_action_2 =
+  fun e ->
+    (
+# 32 "kawaparser.mly"
+                                    ( Print(e) )
+# 109 "kawaparser.ml"
+     : (Kawa.instr))
+
+let _menhir_action_3 =
+  fun e ->
+    (
+# 33 "kawaparser.mly"
+                           ( Return(e) )
+# 117 "kawaparser.ml"
+     : (Kawa.instr))
+
+let _menhir_action_4 =
+  fun () ->
+    (
+# 208 "<standard.mly>"
+    ( [] )
+# 125 "kawaparser.ml"
+     : (Kawa.seq))
+
+let _menhir_action_5 =
+  fun x xs ->
+    (
+# 210 "<standard.mly>"
+    ( x :: xs )
+# 133 "kawaparser.ml"
+     : (Kawa.seq))
+
+let _menhir_action_6 =
+  fun main ->
+    (
+# 28 "kawaparser.mly"
+    ( {classes=[]; globals=[]; main} )
+# 141 "kawaparser.ml"
+     : (Kawa.program))
+
+let _menhir_print_token : token -> string =
+  fun _tok ->
+    match _tok with
+    | ATTRIBUTE ->
+        "ATTRIBUTE"
+    | BEGIN ->
+        "BEGIN"
+    | BOOL _ ->
+        "BOOL"
+    | CLASS ->
+        "CLASS"
+    | ELSE ->
+        "ELSE"
+    | END ->
+        "END"
+    | EOF ->
+        "EOF"
+    | FALSE ->
+        "FALSE"
+    | IDENT _ ->
+        "IDENT"
+    | IF ->
+        "IF"
+    | INT _ ->
+        "INT"
+    | LPAR ->
+        "LPAR"
+    | MAIN ->
+        "MAIN"
+    | METHOD ->
+        "METHOD"
+    | NEW ->
+        "NEW"
+    | PRINT ->
+        "PRINT"
+    | RETURN ->
+        "RETURN"
+    | RPAR ->
+        "RPAR"
+    | SEMI ->
+        "SEMI"
+    | THIS ->
+        "THIS"
+    | TRUE ->
+        "TRUE"
+    | VAR ->
+        "VAR"
+    | VOID ->
+        "VOID"
+    | WHILE ->
+        "WHILE"
+
+let _menhir_fail : unit -> 'a =
+  fun () ->
+    Printf.eprintf "Internal failure -- please contact the parser generator's developers.\n%!";
+    assert false
+
+include struct
+  
+  [@@@ocaml.warning "-4-37"]
+  
+  let _menhir_run_12 : type  ttv_stack. ttv_stack -> _ -> _ -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v ->
+      let _tok = _menhir_lexer _menhir_lexbuf in
+      match (_tok : MenhirBasics.token) with
+      | EOF ->
+          let main = _v in
+          let _v = _menhir_action_6 main in
+          MenhirBox_program _v
+      | _ ->
+          _eRR ()
+  
+  let rec _menhir_run_16 : type  ttv_stack. (ttv_stack, _menhir_box_program) _menhir_cell1_instruction -> _ -> _ -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v ->
+      let MenhirCell1_instruction (_menhir_stack, _menhir_s, x) = _menhir_stack in
+      let xs = _v in
+      let _v = _menhir_action_5 x xs in
+      _menhir_goto_list_instruction_ _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s
+  
+  and _menhir_goto_list_instruction_ : type  ttv_stack. ttv_stack -> _ -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s ->
+      match _menhir_s with
+      | MenhirState15 ->
+          _menhir_run_16 _menhir_stack _menhir_lexbuf _menhir_lexer _v
+      | MenhirState02 ->
+          _menhir_run_12 _menhir_stack _menhir_lexbuf _menhir_lexer _v
+      | _ ->
+          _menhir_fail ()
+  
+  let rec _menhir_run_03 : type  ttv_stack. ttv_stack -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _menhir_s ->
+      let _menhir_stack = MenhirCell1_RETURN (_menhir_stack, _menhir_s) in
+      let _menhir_s = MenhirState03 in
+      let _tok = _menhir_lexer _menhir_lexbuf in
+      match (_tok : MenhirBasics.token) with
+      | INT _v ->
+          _menhir_run_04 _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s
+      | _ ->
+          _eRR ()
+  
+  and _menhir_run_04 : type  ttv_stack. ttv_stack -> _ -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s ->
+      let _tok = _menhir_lexer _menhir_lexbuf in
+      let n = _v in
+      let _v = _menhir_action_1 n in
+      _menhir_goto_expression _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s _tok
+  
+  and _menhir_goto_expression : type  ttv_stack. ttv_stack -> _ -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s _tok ->
+      match _menhir_s with
+      | MenhirState08 ->
+          _menhir_run_09 _menhir_stack _menhir_lexbuf _menhir_lexer _v _tok
+      | MenhirState03 ->
+          _menhir_run_05 _menhir_stack _menhir_lexbuf _menhir_lexer _v _tok
+      | _ ->
+          _menhir_fail ()
+  
+  and _menhir_run_09 : type  ttv_stack. (ttv_stack, _menhir_box_program) _menhir_cell1_PRINT -> _ -> _ -> _ -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _tok ->
+      match (_tok : MenhirBasics.token) with
+      | RPAR ->
+          let _tok = _menhir_lexer _menhir_lexbuf in
+          (match (_tok : MenhirBasics.token) with
+          | SEMI ->
+              let _tok = _menhir_lexer _menhir_lexbuf in
+              let MenhirCell1_PRINT (_menhir_stack, _menhir_s) = _menhir_stack in
+              let e = _v in
+              let _v = _menhir_action_2 e in
+              _menhir_goto_instruction _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s _tok
+          | _ ->
+              _eRR ())
+      | _ ->
+          _eRR ()
+  
+  and _menhir_goto_instruction : type  ttv_stack. ttv_stack -> _ -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s _tok ->
+      let _menhir_stack = MenhirCell1_instruction (_menhir_stack, _menhir_s, _v) in
+      match (_tok : MenhirBasics.token) with
+      | RETURN ->
+          _menhir_run_03 _menhir_stack _menhir_lexbuf _menhir_lexer MenhirState15
+      | PRINT ->
+          _menhir_run_07 _menhir_stack _menhir_lexbuf _menhir_lexer MenhirState15
+      | END ->
+          let _v_0 = _menhir_action_4 () in
+          _menhir_run_16 _menhir_stack _menhir_lexbuf _menhir_lexer _v_0
+      | _ ->
+          _eRR ()
+  
+  and _menhir_run_07 : type  ttv_stack. ttv_stack -> _ -> _ -> (ttv_stack, _menhir_box_program) _menhir_state -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _menhir_s ->
+      let _menhir_stack = MenhirCell1_PRINT (_menhir_stack, _menhir_s) in
+      let _tok = _menhir_lexer _menhir_lexbuf in
+      match (_tok : MenhirBasics.token) with
+      | LPAR ->
+          let _menhir_s = MenhirState08 in
+          let _tok = _menhir_lexer _menhir_lexbuf in
+          (match (_tok : MenhirBasics.token) with
+          | INT _v ->
+              _menhir_run_04 _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s
+          | _ ->
+              _eRR ())
+      | _ ->
+          _eRR ()
+  
+  and _menhir_run_05 : type  ttv_stack. (ttv_stack, _menhir_box_program) _menhir_cell1_RETURN -> _ -> _ -> _ -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer _v _tok ->
+      match (_tok : MenhirBasics.token) with
+      | SEMI ->
+          let _tok = _menhir_lexer _menhir_lexbuf in
+          let MenhirCell1_RETURN (_menhir_stack, _menhir_s) = _menhir_stack in
+          let e = _v in
+          let _v = _menhir_action_3 e in
+          _menhir_goto_instruction _menhir_stack _menhir_lexbuf _menhir_lexer _v _menhir_s _tok
+      | _ ->
+          _eRR ()
+  
+  let _menhir_run_00 : type  ttv_stack. ttv_stack -> _ -> _ -> _menhir_box_program =
+    fun _menhir_stack _menhir_lexbuf _menhir_lexer ->
+      let _tok = _menhir_lexer _menhir_lexbuf in
+      match (_tok : MenhirBasics.token) with
+      | MAIN ->
+          let _tok = _menhir_lexer _menhir_lexbuf in
+          (match (_tok : MenhirBasics.token) with
+          | BEGIN ->
+              let _tok = _menhir_lexer _menhir_lexbuf in
+              (match (_tok : MenhirBasics.token) with
+              | RETURN ->
+                  _menhir_run_03 _menhir_stack _menhir_lexbuf _menhir_lexer MenhirState02
+              | PRINT ->
+                  _menhir_run_07 _menhir_stack _menhir_lexbuf _menhir_lexer MenhirState02
+              | END ->
+                  let _v = _menhir_action_4 () in
+                  _menhir_run_12 _menhir_stack _menhir_lexbuf _menhir_lexer _v
+              | _ ->
+                  _eRR ())
+          | _ ->
+              _eRR ())
+      | _ ->
+          _eRR ()
+  
+end
+
+let program =
+  fun _menhir_lexer _menhir_lexbuf ->
+    let _menhir_stack = () in
+    let MenhirBox_program v = _menhir_run_00 _menhir_stack _menhir_lexbuf _menhir_lexer in
+    v
---
+++ kawa/Kawa/_build/default/kawaparser.mli
@@ -0,0 +1,36 @@
+
+(* The type of tokens. *)
+
+type token = 
+  | WHILE
+  | VOID
+  | VAR
+  | TRUE
+  | THIS
+  | SEMI
+  | RPAR
+  | RETURN
+  | PRINT
+  | NEW
+  | METHOD
+  | MAIN
+  | LPAR
+  | INT of (int)
+  | IF
+  | IDENT of (string)
+  | FALSE
+  | EOF
+  | END
+  | ELSE
+  | CLASS
+  | BOOL of (bool)
+  | BEGIN
+  | ATTRIBUTE
+
+(* This exception is raised by the monolithic API functions. *)
+
+exception Error
+
+(* The monolithic API. *)
+
+val program: (Lexing.lexbuf -> token) -> Lexing.lexbuf -> (Kawa.program)
